0cfa98417000a1d03f7edefda879d90af8984c42
Kerosin3 <keromvp@gmail.com>
2021-08-18 17:58:55 +0300

    added plotting function

--- application/application/models/db_functions_plotting.py
+++ application/application/models/db_functions_plotting.py
@@ -10,46 +10,74 @@ from math import pi
 from bokeh.models import Circle, ColumnDataSource, LinearAxis, Plot, Range1d, Title
 from sqlalchemy.orm import Session,sessionmaker,load_only
 from sqlalchemy import create_engine
+from bokeh.models import LinearAxis, Range1d, Segment, Legend
 
 def top_func(func):
     def wrapper(ticker:str):
-        # ticker = 'AAPL'
         df = func(ticker)
         last = df.index[-1]
-        duration = 90 #sessions
-        before = last - duration
-        df = df.truncate(before=before,after=last) #only - days!
-        # print('last',last)
-        # print('------------------------',df)
-        df["date"] = pd.to_datetime(df["date"])
-        inc = df.close > df.open
-        dec = df.open > df.close
+        duration = -90  # sessions
+        sessions = abs(duration)
+        before = df.index[duration]
+        df = df.truncate(before=before, after=last)  # only - days!
         w = 12 * 60 * 60 * 1000  # half day in ms
         TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
-        p = figure( tools=TOOLS, sizing_mode="stretch_both", title=ticker + " candlestick"+' throught ' + str(duration) +' sessions ')
-        # p = figure( tools=TOOLS, plot_height=300, plot_width=1400, title=ticker + " candlestick")
-        # df['pandas_SMA_3'] = df.iloc[:, 1].rolling(window=3).mean()
-        # p.xaxis.major_label_orientation = pi / 4
-        # p.grid.grid_line_alpha = 1
-        # print(pd.to_datetime(df["date"]))
-        p.add_layout(LinearAxis(), 'right')
-        p.xaxis.major_label_overrides = {
-            i: date.strftime('%b %d') for i, date in enumerate(pd.to_datetime(df["date"]),start=before)
-        }
-        p.xaxis.bounds = (before, last)
+        p = figure(tools=TOOLS, plot_height=350, sizing_mode="stretch_width",
+                   title=ticker + " candlestick" + ' throught ' + str(sessions) + ' sessions ')
+
+        p.add_layout(Legend(click_policy="hide", orientation='horizontal', spacing=20), 'below')
+        p.yaxis.axis_label = 'Price'
+        df.reset_index(inplace=True)
+        df.rename(columns={'index': 'date'}, inplace=True)
+        low, high = df[['open', 'close']].min().min(), df[['open', 'close']].max().max()
+        diff = high - low
+        p.y_range = Range1d(low - 0.1 * diff, high + 0.1 * diff)
+        inc = df.close > df.open
+        dec = df.open > df.close
+        p.xaxis.bounds = (0, sessions)
+        print(pd.to_datetime(df["date"]))
         p.x_range.range_padding = 0.05
-
-        p.segment(df.index, df.high, df.index, df.low, color="black")
-        p.vbar(df.index[inc], 0.5, df.open[inc], df.close[inc], fill_color="#00FF5E", line_color="black")
-        p.vbar(df.index[dec], 0.5, df.open[dec], df.close[dec], fill_color="#F2583E", line_color="black")
-        # p.vbar(df.date[inc], w, df.open[inc], df.close[inc], fill_color="#00FF5E", line_color="black")
-        # p.vbar(df.date[dec], w, df.open[dec], df.close[dec], fill_color="#F2583E", line_color="black")
-        p.xaxis.axis_label = 'Date'
-        p.yaxis.axis_label = 'Price ($)'
-
-        # p.add_layout(Title(text="Bottom Centered Title", align="center"), "below")
-        # output_file("candlestick.html", title="candlestick.py example")
+        p.xaxis.major_label_overrides = {
+            i: date.strftime('%b %d') for i, date in enumerate(pd.to_datetime(df["date"]))
+        }
+        print(df)
+        print('============', inc)
+        p.segment(df.index, df.high, df.index, df.low, color="black", legend_label='Candlestick')
+        p.vbar(df.index[inc], 0.5, df.open[inc], df.close[inc],
+               fill_color="#D5E1DD", line_color="black", legend_label='Candlestick')
+        p.vbar(df.index[dec], 0.5, df.open[dec], df.close[dec],
+               fill_color="#F2583E", line_color="black", legend_label='Candlestick')
+        # right y axis
+        p.extra_y_ranges.update({'two': Range1d(0, 1.1 * df.volume.max())})
+        p.add_layout(LinearAxis(y_range_name='two', axis_label='Volume'), 'right')
+        p.vbar(df.index, 0.5, df.volume, [0] * df.shape[0], alpha=0.5, level='underlay',
+               legend_label='Volume', y_range_name='two')
         return p
+        # last = df.index[-1]
+        # duration = 90 #sessions
+        # before = last - duration
+        # df = df.truncate(before=before,after=last) #only - days!
+        # df["date"] = pd.to_datetime(df["date"])
+        # inc = df.close > df.open
+        # dec = df.open > df.close
+        # w = 12 * 60 * 60 * 1000  # half day in ms
+        # TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
+        # p = figure( tools=TOOLS, plot_height=350,sizing_mode="stretch_width", title=ticker + " candlestick"+' throught ' + str(duration) +' sessions ')
+        # print(pd.to_datetime(df["date"]))
+        # p.add_layout(LinearAxis(), 'right')
+        # print('before',before,last)
+        # p.xaxis.major_label_overrides = {
+        #     i: date.strftime('%b %d') for i, date in enumerate(pd.to_datetime(df["date"]),start=before)
+        # }
+        # p.xaxis.bounds = (before, last)
+        # p.x_range.range_padding = 0.05
+        # p.segment(df.index, df.high, df.index, df.low, color="black")
+        # p.vbar(df.index[inc], 0.5, df.open[inc], df.close[inc], fill_color="#00FF5E", line_color="black")
+        # p.vbar(df.index[dec], 0.5, df.open[dec], df.close[dec], fill_color="#F2583E", line_color="black")
+        # p.extra_y_ranges.update({'two': Range1d(0, 1.1 * df.volume.max())})
+        # p.xaxis.axis_label = 'Date'
+        # p.yaxis.axis_label = 'Price ($)'
+        # return p
     return wrapper
 
 @top_func
